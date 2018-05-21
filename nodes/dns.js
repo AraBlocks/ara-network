a4191a6d99a745ff9e6c159b3108ab0226ad9a3e
Joseph Werle <joseph.werle@gmail.com>
2018-05-21 11:58:32 -0400

    refactor(nodes/dns.js): Clean up module, add program opts, more doc

--- nodes/dns.js
+++ nodes/dns.js
@@ -2,76 +2,127 @@
 
 const { info, warn, error } = require('ara-console')
 const extend = require('extend')
-const debug = require('debug')('ara:network:node:dns-discovery')
+const debug = require('debug')('ara:network:node:dns')
 const dns = require('../dns')
-const os = require('os')
 
 /**
- * Configuration for a DNS discovery server.
+ * Configuration for a DNS server.
  */
 const conf = {
-  workers: os.cpus().length,
-  ports: [5300],
+  multicast: true,
+  loopback: true,
+  ports: [53, 5300],
 }
 
 /**
- * DNS server instance
+ * DNS server server
  */
-let instance = null
+let server = null
 
 /**
- * Starts a DNS discovery server node
+ * Starts a DNS server node
+ * @public
+ * @param {Object} argv
+ * @return Boolean
  */
-async function start(opts) {
-  if (instance) { return false }
-  if (opts && 'object' == typeof opts) {
-    configure(opts)
+async function start(argv) {
+  if (server) { return false }
+
+  info("dns: Starting server")
+
+  server = dns.createServer(conf)
+  server.on('error', onerror)
+  server.on('traffic', ontraffic)
+  server.on('secrets-rotated', onsecretsrotated)
+  server.listen(conf.ports)
+
+  for (const socket of server._sockets) {
+    socket.on('listening', () => {
+      const { port } = socket.address()
+      info("dns: Listening on port %s", port)
+    })
   }
 
-  info('dns-discovery:', "starting server")
-
-  instance = dns.createServer(conf)
-  instance.listen(conf.ports, onlisten)
-  instance.on('error', onerror)
   return true
-
-  function onlisten() {
-    info("dns-discovery: Listening on ports (%s)", conf.ports)
-  }
 
   function onerror(err) {
     if (err && 'EACCES' == err.code) {
+      const { port } = err
+      conf.ports.splice(conf.ports.indexOf(port), 1)
       return debug("error:", err)
     } else if (true == conf.multicast) {
       if (err && 'EADDRINUSE' == err.code) {
         return debug("error:", err)
       }
     }
-    throw err
+  }
+
+  function ontraffic(type, details) {
+    const { message, peer } = details
+    info("dns: traffic: %s: %s: %s", type, message.type, message.opcode)
+  }
+
+  function onsecretsrotated() {
+    info("dns: Secrets rotated")
   }
 }
 
-async function stop() {
-  if (null == instance) { return false }
-  instance.destroy(ondestroy)
+/**
+ * Stops a DNS server node
+ * @public
+ * @return Boolean
+ */
+async function stop(argv) {
+  if (null == server) { return false }
+  warn("dns: Stopping server")
+  server.destroy(ondestroy)
   return true
   function ondestroy() {
-    instance = null
-    process.nextTick(() => process.exit(0))
+    server = null
   }
 }
 
-async function configure(opts) {
+/**
+ * Configures a DNS server node
+ * @public
+ * @param {Object} opts
+ * @return Object
+ */
+async function configure(opts, program) {
+  if (program) {
+    program
+      .option('port', {
+        alias: 'p',
+        type: 'number',
+        describe: "Port or ports to listen on",
+        default: conf.ports
+      })
+      .option('loopback', {
+        type: 'boolean',
+        default: conf.loopback,
+        describe: "Loopback DNS discovery",
+      })
+      .option('multicast', {
+        type: 'boolean',
+        default: conf.multicast,
+        describe: "Multicast DNS discovery",
+      })
+  }
   return extend(true, conf, opts)
 }
 
-function getInstance() {
-  return instance
+/**
+ * Returns reference to DNS server node
+ * @public
+ * @return Object
+ */
+async function getInstance(argv) {
+  return server
 }
 
 module.exports = {
+  getInstance,
+  configure,
   start,
   stop,
-  configure,
-  getInstance,
 }
