9d7bc1ea2f741f66aaef03e679d0dbc595aa813e
Joseph Werle <joseph.werle@gmail.com>
2018-05-26 23:43:14 -0400

    refactor(secret-handshake.js): Use simple secret handshake instead

--- secret-handshake.js
+++ secret-handshake.js
@@ -1,103 +1,60 @@
 'use strict'
 
 const isBuffer = require('is-buffer')
-const pull = require('pull-stream')
-const shs = require('secret-handshake')
+const secrets = require('./secrets')
+const crypto = require('ara-crypto')
 
-/**
- * Pull (pipe) a source stream into a destination
- * stream piped back into the source stream.
- * @public
- * @param {Stream} source
- * @param {Stream} destination
- * @return {Stream}
- */
-function connect(source, destination) {
-  return pull(source, destination, source)
-}
+class Handshake {
+  constructor(opts) {
+    if (null == opts || 'object' != typeof opts) {
+      throw new TypeError("Handshake: Expecting object.")
+    }
 
-/**
- * Creates a secret handshake server for a
- * client with key pair and a given network key.
- * @public
- * @param {Object} opts
- * @param {Function} opts.onauthorize
- * @param {Object} opts.client
- * @param {Object} opts.client.publicKey
- * @param {Object} opts.client.secretKey
- * @param {Object} opts.network
- * @param {Object} opts.network.key
- * @return {Object}
- * @throws TypeError
- */
-function createServer(opts) {
-  if (null == opts || 'object' != typeof opts) {
-    throw new TypeError(
-      "ara-network.secret-handshake.createServer: Expecting object.")
+    const { remote, client, network } = opts
+    this.remote = remote
+    this.client = client
+    this.network = network
   }
 
-  if (null == opts.client || 'object' != typeof opts.client) {
-    throw new TypeError(
-      "ara-network.secret-handshake.createServer: Expecting client to be an object.")
+  iv() {
+    return crypto.randomBytes(16)
   }
 
-  if (null == opts.network || 'object' != typeof opts.network) {
-    throw new TypeError(
-      "ara-network.secret-handshake.createServer: Expecting network to be an object.")
+  key() {
+    return this.network.key.slice(0, 16)
   }
 
-  if (false == isBuffer(opts.network.key)) {
-    throw new TypeError(
-      "ara-network.secret-handshake.createServer: Expecting network key to be a buffer.")
+  digest() {
+    const buffers = []
+    buffers.push(this.network.key)
+    buffers.push(this.remote.secretKey)
+    buffers.push(this.client.secretKey)
+    return crypto.blake2b(Buffer.concat(buffers))
   }
 
-  const { client, onauthorize, network } = opts
-  const server = shs.createServer(client, onauthorize, network.key)
-  return server
-}
-
-/**
- * Creates a secret handshake client for a
- * remote with keys and a given network key.
- * @public
- * @param {Object} opts
- * @param {Function} opts.onauthorize
- * @param {Object} opts.remote
- * @param {Object} opts.remote.publicKey
- * @param {Object} opts.remote.secretKey
- * @param {Object} opts.network
- * @param {Object} opts.network.key
- * @return {Object}
- * @throws TypeError
- */
-function createClient(opts) {
-  if (null == opts || 'object' != typeof opts) {
-    throw new TypeError(
-      "ara-network.secret-handshake.createClient: Expecting object.")
+  challenge() {
+    const iv = this.iv()
+    const key = this.key()
+    const digest = this.digest()
+    const challenge = crypto.encrypt(digest, {key, iv})
+    return Buffer.from(JSON.stringify(challenge), 'utf8')
   }
 
-  if (null == opts.remote || 'object' != typeof opts.remote) {
-    throw new TypeError(
-      "ara-network.secret-handshake.createClient: Expecting remote to be an object.")
+  proof(challenge) {
+    if (isBuffer(challenge)) {
+      challenge = JSON.parse(challenge.toString('utf8'))
+    }
+    const key = this.key()
+    const digest = this.digest()
+    const proof = crypto.decrypt(challenge, {key})
+    if (0 == Buffer.compare(digest, proof)) {
+      return true
+    } else {
+      return false
+    }
   }
-
-  if (null == opts.network || 'object' != typeof opts.network) {
-    throw new TypeError(
-      "ara-network.secret-handshake.createClient: Expecting network to be an object.")
-  }
-
-  if (false == isBuffer(opts.network.key)) {
-    throw new TypeError(
-      "ara-network.secret-handshake.createClient: Expecting network key to be a buffer.")
-  }
-
-  const { remote, network } = opts
-  const client = shs.createClient(remote, network.key)
-  return client
 }
 
 module.exports = {
-  createServer,
-  createClient,
-  connect,
+  Handshake
 }
