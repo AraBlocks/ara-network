d1c35cba2d24dc17dad39e7f700796844bf66b76
Joseph Werle <joseph.werle@gmail.com>
2018-06-02 14:31:16 -0400

    refactor(index.js): Ensure modules are exported at index

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 'use strict'
 
+const { Handshake } = require('./secret-handshake')
 const discovery = require('./discovery')
 const signalhub = require('./signalhub')
+const channel = require('./channel')
+const secrets = require('./secrets')
 const dht = require('./dht')
 const dns = require('./dns')
 
 module.exports = {
+  Handshake,
   discovery,
   signalhub,
+  channel,
+  secrets,
   dht,
   dns,
 }
