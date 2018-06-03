8f373d2aebf64c1ff7808a7a6a5fcf024b234346
Joseph Werle <joseph.werle@gmail.com>
2018-06-03 18:47:10 -0400

    feat(rc.js): Add runtime configuration

---
+++ rc.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const { resolve } = require('path')
+const extend = require('extend')
+const rc = require('ara-runtime-configuration')
+
+const defaults = {
+  network: {
+    secrets: {
+      root: resolve(rc().data.root, 'secrets')
+    }
+  }
+}
+
+module.exports = (conf) => rc(extend(true, {}, conf, defaults))
