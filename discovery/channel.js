8078a7de8f922cd8f040cb0ab23b3ab3d1a1e474
Joseph Werle <joseph.werle@gmail.com>
2018-06-10 19:16:51 -0400

    wip

--- discovery/channel.js
+++ discovery/channel.js
@@ -6,8 +6,13 @@ const crypto = require('ara-crypto')
 const debug = require('debug')('ara:network:channel')
 
 const defaults = {
-  dns: require('./dns').defaults,
-  dht: require('./dht').defaults,
+  hash: false,
+  dns: require('../dns').defaults,
+  dht: require('../dht').defaults,
+
+  get id() {
+    return crypto.randomBytes(32)
+  },
 }
 
 /**
@@ -20,6 +25,7 @@ const defaults = {
 function createChannel(opts) {
   if (null == opts || 'object' != typeof opts) { opts = {} }
   opts = extend(true, {}, defaults, opts)
+  console.log(opts);
   const channel = discovery(opts)
   return channel
 }
--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 const { Handshake } = require('./secret-handshake')
 const discovery = require('./discovery')
 const signalhub = require('./signalhub')
-const channel = require('./channel')
 const secrets = require('./secrets')
 const dht = require('./dht')
 const dns = require('./dns')
@@ -12,7 +11,6 @@ module.exports = {
   Handshake,
   discovery,
   signalhub,
-  channel,
   secrets,
   dht,
   dns,
