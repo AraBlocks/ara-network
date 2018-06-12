ee6e173143b624c87bd99eaf2b20865f4935271d
Madeline Cameron <code@maddie.today>
2018-06-12 14:52:10 -0400

    docs(): Add code documentation

--- secrets.js
+++ secrets.js
@@ -13,8 +13,8 @@ const NETWORK = 1
 const REMOTE = 2
 const CLIENT = 3
 
-const PKX = Buffer.from('PKX') // public keystore header
-const SKX = Buffer.from('SKX') // secret keystore header
+const PKX = Buffer.from('PKX')  // public keystore header
+const SKX = Buffer.from('SKX')  // secret keystore header
 
 const toHex = (b) => b && b.toString('hex')
 
@@ -39,12 +39,16 @@ function ensureKeyPair(keyPair) {
  * a keystore and discovery key. The keystore stores
  * secret keys for the network and remote/client connections
  * wishing to communicate through a secret handshake.
+ *
  * @public
- * @param {?(Object)} opts
- * @param {?(String|Buffer)} opts.key
- * @param {?(String|Buffer)} opts.seed
+ *
+ * @param {Object} opts
+ * @param {(String|Buffer)} opts.key
+ * @param {(String|Buffer)} [opts.seed]
+ *
  * @return {Object}
  */
+
 function encrypt(opts) {
   const keys = {}
 
@@ -95,6 +99,22 @@ function encrypt(opts) {
     return buffer
   }
 }
+
+/**
+ * Create encrypted document of keys
+ *
+ * @param  {Object} keys
+ * @param  {Object} keys.discoveryKey
+ * @param  {Object} keys.network.publicKey
+ * @param  {Object} keys.network.secretKey
+ * @param  {Object} keys.remote.publicKey
+ * @param  {Object} keys.remote.secretKey
+ * @param  {Object} keys.client.secretKey
+ * @param  {Object} opts
+ * @param  {Object} opts.key Encryption key
+ *
+ * @return {[type]}      [description]
+ */
 
 function pack(keys, opts) {
   const freelist = []
@@ -152,17 +172,38 @@ function pack(keys, opts) {
 
   return result
 
+  /**
+   * Create buffer of specific size
+   *
+   * @private
+   *
+   * @param  {Number} size Size of buffer
+   * @param  {Buffer} mem  Initial content
+   *
+   * @return {Buffer}
+   */
+
   function alloc(size, mem) {
     let buffer = null
-    if (Buffer.isBuffer(size)) { buffer = size }
-    else { buffer = Buffer.allocUnsafe(size).fill(mem || 0) }
-    if (false == freelist.includes(buffer)) { freelist.push(buffer) }
+
+    if (Buffer.isBuffer(size)) buffer = size
+    else buffer = Buffer.allocUnsafe(size).fill(mem || 0)
+
+    if (false == freelist.includes(buffer)) freelist.push(buffer)
+
     return buffer
   }
 
+  /**
+   * Overwrite buffer with 0s
+   *
+   * @private
+   */
+
   function free() {
     let buffer = null
-    while (buffer = freelist.shift()) { buffer.fill(0) }
+
+    while (buffer = freelist.shift()) buffer.fill(0)
   }
 }
 
@@ -182,10 +223,13 @@ function pack(keys, opts) {
  * 4 - 64 byte client private key
  *
  * @public
+ *
  * @param {Object} doc
  * @param {Object} opts
+ *
  * @return {Object}
  */
+
 function decrypt(doc, opts) {
   if (false == Buffer.isBuffer(opts.key)) {
     opts.key = Buffer(opts.key)
@@ -242,10 +286,14 @@ function decrypt(doc, opts) {
 /**
  * Load secrets by name from the secrets root directory.
  * Secrets are indexed by the blake2b hash of their value.
+ *
  * @public
+ *
  * @param {Object} opts
+ *
  * @return {Object}
  */
+
 async function load(opts) {
   if (null == opts || 'object' != typeof opts) {
     throw new TypeError("load: Expecting object.")
@@ -285,11 +333,15 @@ async function load(opts) {
 /**
  * Derive a resolution document from a secret containing
  * public and secret keys if available.
+ *
  * @public
+ *
  * @param {Object} secret
  * @param {Object} opts
+ *
  * @return {Object}
  */
+
 function derive(secret, opts) {
   const doc = decrypt(secret, opts)
   const result = { public: null, secret: null, }
--- signalhub.js
+++ signalhub.js
@@ -2,6 +2,14 @@
 
 const Server = require('signalhub/server')
 
+/**
+ * Create Signalhub Server (https://github.com/mafintosh/signalhub)
+ *
+ * @param  {Object} opts
+ *
+ * @return {Signalhub}
+ */
+
 function createServer(opts) {
   const server = Server(opts)
   return server
