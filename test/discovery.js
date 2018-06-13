bfee11aeaeb2e98d173a2e422cb02a080446c449
Madeline Cameron <github@maddie.today>
2018-06-13 16:28:44 -0400

    test(): Add basic tests

---
+++ test/discovery.js
@@ -0,0 +1,40 @@
+const { createServer, createChannel, createSwarm } = require('../discovery')
+const discoveryChannel = require('discovery-channel')
+const discoverySwarm = require('discovery-swarm')
+const { test } = require('ava')
+
+test('discovery - createSwarm - no opts', (t) => {
+  const server = createSwarm()
+
+  t.true(server instanceof discoverySwarm)
+})
+
+test('discovery - createSwarm - opts', (t) => {
+  const server = createSwarm({})
+
+  t.true(server instanceof discoverySwarm)
+})
+
+test('discovery - createChannel - no opts', (t) => {
+  const client = createChannel()
+
+  t.true(client instanceof discoveryChannel)
+})
+
+test('discovery - createChannel - opts', (t) => {
+  const client = createChannel({})
+
+  t.true(client instanceof discoveryChannel)
+})
+
+test('discovery - createServer - no opts', (t) => {
+  const server = createServer()
+
+  t.true(server instanceof discoverySwarm)
+})
+
+test('discovery - createServer - opts', (t) => {
+  const server = createServer({})
+
+  t.true(server instanceof discoverySwarm)
+})
