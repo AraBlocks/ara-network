bef72c4efdc4a7799a9d25ce08d074aa2a6aa7d1
Madeline Cameron <github@maddie.today>
2018-06-13 16:31:44 -0400

    refactor(discovery/index.js): Remove unneeded spaces

--- discovery/index.js
+++ discovery/index.js
@@ -1,11 +1,8 @@
-
-
 const { createChannel } = require('./channel')
 const { createSwarm } = require('./swarm')
 
 module.exports = {
   createServer: createSwarm,
-
   createChannel,
   createSwarm,
 }
