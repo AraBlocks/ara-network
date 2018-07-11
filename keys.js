3d58f49b99c0e64b93f4f29fe53ec0b5f5eff9ab
Joseph Werle <joseph.werle@gmail.com>
2018-07-11 18:23:49 -0400

    refactor(keys.js): Add encrypt,decrypt,keyPair

--- keys.js
+++ keys.js
@@ -3,6 +3,7 @@ const crypto = require('ara-crypto')
 
 /**
  * Supported packed binary formats in this module
+ * @public
  */
 const VERSION0 = 0x0
 const VERSION = VERSION0
@@ -13,12 +14,16 @@ const VERSION = VERSION0
  *
  * `SKX` is the magic byte prepended to packed secret network
  * keys binary.
+ *
+ * @public
  */
 const PKX = 0x70
 const SKX = 0x73
 
 /**
  * Header sizes for packed binary
+ *
+ * @public
  */
 const HEADER_SIZE0 = 1 + 1 + 32 + 32
 const HEADER_SIZE = HEADER_SIZE0
@@ -26,6 +31,8 @@ const HEADER_SIZE = HEADER_SIZE0
 /**
  * Public and network network key sizes,
  * including the header size.
+ *
+ * @public
  */
 const PK_SIZE0 = HEADER_SIZE0 + 32
 const SK_SIZE0 = HEADER_SIZE0 + 32
@@ -35,14 +42,10 @@ const SK_SIZE = SK_SIZE0
 /**
  * Pack keys into a compact buffer.
  *
- *   pack(T, V, D, B, K) = T|V|D|B_public_|K(T)
- *     where
- *     K(T) = 0x73 === T ? K_secret_ : K_public_
- *
  * @public
- * @see {pack0}
+ * @see {@link pack0}
  * @param {Object} opts
- * @param {?(Number)} opts.version
+ * @param {?(Number)} [opts.version = VERSION]
  * @return {Buffer}
  * @throws TypeError
  */
@@ -69,13 +72,16 @@ function pack(opts) {
 }
 
 /**
- * Version `0x0` binary packing format
+ * Version `0` binary packing format
+ *   pack(T, V, D, B, K) = T|V|D|B_public_|K(T)
+ *     where
+ *     K(T) = 0x73 === T ? K_secret_ : K_public_
+ *
  * @public
  * @param {Object} opts
  * @param {Number} opts.type
  * @param {Object} opts.domain
  * @param {Object} opts.publicKey
- * @param {?(Number)} opts.version
  * @param {Buffer} opts.discoveryKey
  * @return {Buffer}
  * @throws TypeError
@@ -96,8 +102,9 @@ function pack0(opts) {
     throw unknownTypeError('pack', T)
   }
 
-  buffer.push(Buffer.from([T])) // 1
-  buffer.push(Buffer.from([V])) // 1
+  // + 1 + 1
+  buffer.push(Buffer.from([T]))
+  buffer.push(Buffer.from([V]))
 
   if (!D || false === isBuffer(D)) {
     throw new TypeError('pack: Invalid discovery key')
@@ -105,7 +112,8 @@ function pack0(opts) {
     throw new TypeError(`pack: Bad discovery key length: ${D.length}`)
   }
 
-  buffer.push(D) // 32
+  // + 32
+  buffer.push(D)
 
   if (!B || false === isBuffer(B)) {
     throw new TypeError('pack: Invalid public key pair')
@@ -113,14 +121,17 @@ function pack0(opts) {
     throw new TypeError(`pack: Bad public key length: ${B.length}`)
   }
 
-  buffer.push(B) // 32
+  // + 32
+  buffer.push(B)
 
   if (!K) {
     throw new TypeError('pack: Missing domain keys')
   } else if (T === SKX && isBuffer(K.secretKey)) {
-    buffer.push(K.secretKey) // 32
+    // + 32
+    buffer.push(K.secretKey)
   } else if (T === PKX && isBuffer(K.publicKey)) {
-    buffer.push(K.publicKey) // 32
+    // + 32
+    buffer.push(K.publicKey)
   } else {
     throw new TypeError('pack: Missing domain key')
   }
@@ -141,7 +152,9 @@ function pack0(opts) {
 
 /**
  * Unpack binary buffer network keys.
+ *
  * @public
+ * @see {@link unpack0}
  * @param {Object} opts
  * @param {Buffer} buffer
  * @return {Object}
@@ -171,6 +184,14 @@ function unpack(opts) {
       throw unknownTypeError('unpack', T)
     }
 
+    if (Number.isNaN(V)) {
+      throw new TypeError(`unpack: Invalid version: ${V}`)
+    } else if (V < 0) {
+      throw new TypeError(`unpack: Version cannot be signed: ${V}`)
+    } else if (V > 0xff) {
+      throw new TypeError(`unpack: Version out of range: 0 <= ${V} < ${0xff}`)
+    }
+
     if (V === VERSION0) {
       const offset = T === PKX ? PK_SIZE0 : SK_SIZE0
       const buffer = Z.slice(0, offset)
@@ -191,7 +212,13 @@ function unpack(opts) {
 }
 
 /**
- * Version `0x0` binary unpacking format
+ * Version `0` binary unpacking format.
+ *   > HEADER ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ > BODY ~~~~~~~~
+ *   | 1 byte      | 1 byte  | 32 bytes  | 32 bytes  | 32 bytes    |
+ *   | ----------- | ------- | --------- | --------- | ----------- |
+ *   | (T=PKX|SKX) |  (V=0)  |  (D=...)  |  (B=...)  |  (K=Kp|Ks)  |
+ *   | ----------- | ------- | --------- | --------- | ----------- |
+ *
  * @public
  * @param {Object} opts
  * @param {Buffer} buffer
@@ -218,12 +245,27 @@ function unpack0(opts) {
     },
   }
 
+  // header= T|V|D|B (1+1+32+32)
   const Z = opts.buffer
 
-  // header= T|V|D|B (1+1+32+32)
-  const T = read(1)[0] // read byte value
-  const V = read(1)[0] // " "
+  // packed binary type
+  const T = read(1)[0]
+
+  if (T !== PKX && T !== SKX) {
+    throw unknownTypeError('unpack', T)
+  }
+
+  // packed binary version
+  const V = read(1)[0]
+
+  if (V !== VERSION0) {
+    throw new TypeError(`unpack: Unsupported version ${V}`)
+  }
+
+  // discovery key
   const D = read(32)
+
+  // public key
   const B = read(32)
 
   unpacked.discoveryKey = D
@@ -238,23 +280,171 @@ function unpack0(opts) {
   return unpacked
 
   function read(size) {
+    /* eslint-disable no-return-assign */
     return Z.slice(read.i, read.i += size)
   }
 }
 
 /**
- * Encrypts generated networks keys into a secret encrypt for some
- * network key pair (B) based on a shared secret key.
+ * Encrypts packed network keys into a secret storage based on
+ * some shared key along with a secret key.
+ * @public
+ * @param {Object} opts
+ * @param {Buffer} opts.buffer
+ * @param {Buffer} opts.secret
+ * @param {Buffer} opts.secretKey
+ * @return {Object}
+ * @throws TypeError
  */
 function encrypt(opts) {
   if (!opts || 'object' !== typeof opts) {
     throw new TypeError('encrypt: Expecting object')
   }
+
+  const Z = opts.buffer
+  const V = Z[1]
+
+  if (Number.isNaN(V)) {
+    throw new TypeError(`encrypt: Invalid version: ${V}`)
+  } else if (V < 0) {
+    throw new TypeError(`encrypt: Version cannot be signed: ${V}`)
+  } else if (V > 0xff) {
+    throw new TypeError(`encrypt: Version out of range: 0 <= ${V} < ${0xff}`)
+  }
+
+  if (V === VERSION0) {
+    return encrypt0(opts)
+  }
+
+  throw new TypeError(`encrypt: Unsupported version ${V}`)
 }
 
 /**
+ * Encrypts packed network keys into a secret storage based on
+ * some shared key along with a secret key for a version 0 binary
+ * format
+ * @public
+ * @param {Object} opts
+ * @param {Buffer} opts.buffer
+ * @param {Buffer} opts.secret
+ * @param {Buffer} opts.secretKey
+ * @return {Object}
+ * @throws TypeError
  */
-function decrypt(opts) {
+function encrypt0(opts) {
+  if (!opts || 'object' !== typeof opts) {
+    throw new TypeError('encrypt: Expecting object')
+  }
+
+  // secret key
+  const Bs = opts.secretKey
+  // constant shared secret
+  const S = opts.secret
+  // public or secret network keys
+  const Z = opts.buffer
+  // magic byte
+  const T = Z[0]
+  // ephemeral shared secret
+  const s = crypto.blake2b(S, 32)
+  // 128-bit initialization vector
+  const iv = crypto.randomBytes(16)
+  // 128-bit encryption key
+  const key = Buffer.allocUnsafe(16)
+
+  if (T === PKX) {
+    // k = blake2b(S')
+    crypto.blake2b(s, 16).copy(key)
+  } else if (T === SKX) {
+    // k = blake2b(S' . Bs)
+    crypto.blake2b(Buffer.concat([s, Bs]), 16).copy(key)
+  } else {
+    throw unknownTypeError('encrypt', T)
+  }
+
+  const result = crypto.encrypt(Z, { key, iv })
+
+  result.type = T
+
+  key.fill(0)
+  iv.fill(0)
+
+  return result
+}
+
+/**
+ * @public
+ * @param {Object} opts
+ * @param {Buffer} opts.buffer
+ * @param {Buffer} opts.secret
+ * @param {Buffer} opts.secretKey
+ * @return {Object}
+ * @throws TypeError
+ */
+function decrypt(storage, opts) {
+  if (!storage || 'object' !== typeof storage) {
+    throw new TypeError('decrypt: Expecting object')
+  } else if (!opts || 'object' !== typeof opts) {
+    throw new TypeError('decrypt: Expecting object')
+  }
+
+  const V = 'number' === typeof opts.version ? opts.version : VERSION
+
+  if (Number.isNaN(V)) {
+    throw new TypeError(`decrypt: Invalid version: ${V}`)
+  } else if (V < 0) {
+    throw new TypeError(`decrypt: Version cannot be signed: ${V}`)
+  } else if (V > 0xff) {
+    throw new TypeError(`decrypt: Version out of range: 0 <= ${V} < ${0xff}`)
+  }
+
+  if (V === VERSION0) {
+    return decrypt0(storage, opts)
+  }
+
+  throw new TypeError(`decrypt: Unsupported version ${V}`)
+}
+
+/**
+ * @public
+ * @param {Object} opts
+ * @param {Buffer} opts.buffer
+ * @param {Buffer} opts.secret
+ * @param {Buffer} opts.secretKey
+ * @return {Object}
+ * @throws TypeError
+ */
+function decrypt0(storage, opts) {
+  if (!storage || 'object' !== typeof storage) {
+    throw new TypeError('decrypt: Expecting object')
+  } else if (!opts || 'object' !== typeof opts) {
+    throw new TypeError('decrypt: Expecting object')
+  }
+
+  const Bs = opts.secretKey
+  const S = opts.secret
+  // derive storage type from options or directly from the storage object
+  const T = opts.type || storage.type
+
+  if (T !== PKX && T !== SKX) {
+    throw unknownTypeError('decrypt', T)
+  }
+
+  const s = crypto.blake2b(S, 32)
+  const key = Buffer.allocUnsafe(16)
+
+  if (T === PKX) {
+    // k = blake2b(S')
+    crypto.blake2b(s, 16).copy(key)
+  } else if (T === SKX) {
+    // k = blake2b(S' . Bs)
+    crypto.blake2b(Buffer.concat([s, Bs]), 16).copy(key)
+  } else {
+    throw unknownTypeError('decrypt', T)
+  }
+
+  const result = crypto.decrypt(storage, { key })
+
+  return result
 }
 
 /**
@@ -342,10 +532,63 @@ function generate0(opts) {
     }),
   ])
 
-  if (Z.length != PK_SIZE0 + SK_SIZE0) {
-    throw new Error("generate: Failed to correctly generate network keys.")
+  if (Z.length !== PK_SIZE0 + SK_SIZE0) {
+    throw new Error('generate: Failed to correctly generate network keys')
   }
+
   return Z
+}
+
+/**
+ * Generate a network keys secret storage key pair.
+ *
+ * @public
+ * @param {Object} opts
+ * @return {Object}
+ * @throws TypeError
+ */
+function keyPair(opts) {
+  if (!opts || 'object' !== typeof opts) {
+    throw new TypeError('keyPair: Expecting object')
+  }
+
+  const V = 'number' === typeof opts.version ? opts.version : VERSION
+
+  if (Number.isNaN(V)) {
+    throw new TypeError(`keyPair: Invalid version: ${V}`)
+  } else if (V < 0) {
+    throw new TypeError(`keyPair: Version cannot be signed: ${V}`)
+  } else if (V > 0xff) {
+    throw new TypeError(`keyPair: Version out of range: 0 <= ${V} < ${0xff}`)
+  }
+
+  if (V === VERSION0) {
+    return keyPair0(opts)
+  }
+
+  throw new TypeError(`keyPair: Unsupported version ${V}`)
+}
+
+/**
+ */
+function keyPair0(opts) {
+  if (!opts || 'object' !== typeof opts) {
+    throw new TypeError('keyPair: Expecting object')
+  }
+
+  const buffer = generate0(opts)
+  const publicKeys = encrypt0({
+    secret: opts.secret,
+    buffer: buffer.slice(0, PK_SIZE0),
+  })
+
+  const secretKeys = encrypt0({
+    secret: opts.secret,
+    buffer: buffer.slice(PK_SIZE0, PK_SIZE0 + SK_SIZE0),
+    secretKey: opts.secretKey,
+  })
+
+  return { publicKeys, secretKeys }
 }
 
 function unknownTypeError(n, T) {
@@ -355,7 +598,15 @@ function unknownTypeError(n, T) {
 module.exports = {
   generate0,
   generate,
+
+  keyPair0,
+  keyPair,
+
+  encrypt0,
   encrypt,
+
+  decrypt0,
+  decrypt,
 
   unpack0,
   unpack,
