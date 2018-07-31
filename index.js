d322c8ccf9a988b313761b13c45d36d7da645461
Joseph Werle <joseph.werle@gmail.com>
2018-07-31 16:29:29 -0400

    refactor(index.js): Export handshake module

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { Handshake } = require('./secret-handshake')
 const discovery = require('./discovery')
 const signalhub = require('./signalhub')
+const handshake = require('./handshake')
 const secrets = require('./secrets')
 const dht = require('./dht')
 const dns = require('./dns')
@@ -9,6 +10,7 @@ module.exports = {
   Handshake,
   discovery,
   signalhub,
+  handshake,
   secrets,
   dht,
   dns,
