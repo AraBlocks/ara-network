f09f445a732276780300dc0bdd965ba091b939ff
Joseph Werle <joseph.werle@gmail.com>
2018-08-03 10:11:26 -0400

    refactor(handshake): Refactor handshake to use isolated source
    Addresses broken handshake protocol duplex stream by introducing an
    isolated source for unboxing so read and write streams created from the
    handshake read and write to the correct sources/sinks.

---
+++ example/handshake/server.js
@@ -0,0 +1,143 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const { decrypt, unpack } = require('../../keys')
+const { createChannel } = require('../../discovery/channel')
+const { info, warn } = require('ara-console')
+const { Handshake } = require('../../handshake')
+const { readFile } = require('fs')
+const { resolve } = require('path')
+const inquirer = require('inquirer')
+const { DID } = require('did-uri')
+const crypto = require('ara-crypto')
+const pify = require('pify')
+const pump = require('pump')
+const net = require('net')
+const rc = require('../../rc')()
+
+require('ara-identity/rc')()
+
+const conf = {}
+let channel = null
+
+async function getInstance() {
+  return channel
+}
+
+async function configure(opts, program) {
+  const { argv } = program
+    .option('identity', {
+      alias: 'i',
+      describe: 'ARA identity for this network node. (Requires password)'
+    })
+    .option('secret', {
+      alias: 's',
+      describe: 'Shared secret key for network keys associated with this node.'
+    })
+    .option('keys', {
+      alias: 'k',
+      describe: 'Path to ARA network keys'
+    })
+    .option('port', {
+      alias: 'p',
+      default: 3000,
+      describe: 'Port this node should connect on.'
+    })
+
+  if (argv.identity && 0 != argv.identity.indexOf('did:ara:')) {
+    argv.identity = `did:ara:${argv.identity}`
+  }
+
+  conf.port = argv.port
+  conf.keys = argv.keys
+  conf.secret = argv.secret
+  conf.identity = argv.identity
+}
+
+async function start() {
+  channel = createChannel({ })
+
+  let { password } = await inquirer.prompt([ {
+    type: 'password',
+    name: 'password',
+    message:
+    'Please enter the passphrase associated with the node identity.\n' +
+    'Passphrase:'
+  } ])
+
+  const did = new DID(conf.identity)
+  const publicKey = Buffer.from(did.identifier, 'hex')
+
+  password = crypto.blake2b(Buffer.from(password))
+
+  const hash = crypto.blake2b(publicKey).toString('hex')
+  const path = resolve(rc.network.identity.root, hash, 'keys')
+  const secret = Buffer.from(conf.secret)
+  const keystore = JSON.parse(await pify(readFile)(path, 'utf8'))
+
+  const secretKey = crypto.decrypt(keystore, { key: password.slice(0, 16) })
+  const keys = decrypt(JSON.parse(await pify(readFile)(conf.keys)), {
+    secret, secretKey
+  })
+
+  const unpacked = unpack({ buffer: keys })
+  const { discoveryKey } = unpacked
+  const server = net.createServer(onconnection)
+
+  server.listen(conf.port, onlisten)
+
+  return true
+
+  function onlisten(err) {
+    if (err) { throw err }
+    const { port } = server.address()
+    channel.join(discoveryKey, port)
+  }
+
+  function onconnection(socket) {
+    const handshake = new Handshake({
+      publicKey,
+      secretKey,
+      secret,
+      remote: { publicKey: unpacked.publicKey },
+      domain: { publicKey: unpacked.domain.publicKey }
+    })
+
+    handshake.on('hello', onhello)
+    handshake.on('auth', onauth)
+    handshake.on('okay', onokay)
+
+    pump(handshake, socket, handshake, (err) => {
+      if (err) {
+        warn(err.message)
+      } else {
+        info('connection closed')
+      }
+    })
+
+    function onhello() {
+      info('got HELLO')
+      handshake.hello()
+    }
+
+    function onauth() {
+      info('got AUTH')
+    }
+
+    function onokay() {
+      info('got OKAY', handshake.state.phase)
+      const reader = handshake.createReadStream()
+      // socket.pipe(process.stdout)
+      reader.pipe(process.stdout)
+    }
+  }
+}
+
+async function stop() {
+  channel.destroy()
+}
+
+module.exports = {
+  getInstance,
+  configure,
+  start,
+  stop,
+}
