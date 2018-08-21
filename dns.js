caa7ebddb8cfaf857d6516a386cae2f030e68bae
Joseph Werle <joseph.werle@gmail.com>
2018-08-21 11:39:56 -0400

    refactor(dns.js): Clean up lint issues

--- dns.js
+++ dns.js
@@ -1,6 +1,6 @@
 const discovery = require('dns-discovery')
 const extend = require('extend')
-const debug = require('debug')('ara:network:dns')
+// const debug = require('debug')('ara:network:dns')
 
 const defaults = {
   multicast: true,
@@ -13,19 +13,20 @@ const defaults = {
  * Create a DNS discovery server
  *
  * @public
- *
  * @param {Object} opts
  * @param {Boolean} [opts.multicast = true]
  * @param {Boolean} [opts.loopback = true]
  * @param {String} [opts.domain = 'ara.local']
  * @param {Number} [opts.limit = 10000]
- *
  * @return {Object}
  */
 
 function createServer(opts) {
-  if (!opts || 'object' !== typeof opts) { opts = {} }
-  debug('creating server')
+  if (!opts || 'object' !== typeof opts) {
+    // eslint-disable-next-line no-param-reassign
+    opts = {}
+  }
+  // eslint-disable-next-line no-param-reassign
   opts = extend(true, {}, defaults, opts)
   const server = discovery(opts)
   return server
