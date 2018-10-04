c99274c9aa16ea3682fae7964e110f0be3de3dec
vipyne <vipyne@gmail.com>
2018-10-04 18:11:20 -0400

    refactor(discovery/{channel,swarm}.js): Add warning when default dht.bootstrap && dns.server are null

--- discovery/swarm.js
+++ discovery/swarm.js
@@ -1,6 +1,7 @@
 const { defaults: dnsDefaults } = require('../dns')
 const { defaults: dhtDefaults } = require('../dht')
 const discovery = require('discovery-swarm')
+const { warn } = require('ara-console')
 const extend = require('extend')
 const crypto = require('ara-crypto')
 const rc = require('../rc')()
@@ -32,6 +33,12 @@ function createSwarm(opts) {
     // eslint-disable-next-line no-param-reassign
     opts = {}
   }
+
+  if (!defaults.dns.server && !defaults.dht.bootstrap) {
+    warn(`No default dht.bootstrap or dns.server value was found.` +
+    `Be sure to use a local archiver | resolver.`)
+  }
+
   // eslint-disable-next-line no-param-reassign
   opts = extend(true, {}, defaults, opts)
   const server = discovery(opts)
